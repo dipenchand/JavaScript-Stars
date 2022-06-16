40fdd8901abfdad594d57d8d55c811d37f942916
dipenchand <personaldipen@gmail.com>
2022-06-16 15:58:24 +0545

    hollow square pattern

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+function hollowSquarePattern (n) {
+    let string = ''
+    for (let i = 0; i < n; i++) {
+        for(let j = 0; j < n; j++) {
+            if (i === 0 || i === n - 1) {
+                string += '*'
+            } else {
+                if (j === 0 || j === n - 1) {
+                    string += '*'
+                } else {
+                    string += ' '
+                }
+            }
+        }
+        string += ' \n'
+    }
+    console.log(string)
+}
+
+hollowSquarePattern(10)
+
 function triangleStar(n) {
     let string = ''
     for (let i = 1; i <= n; i++) {
